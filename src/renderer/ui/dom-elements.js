1f32a8bd588285ca5705940b2641c89c110a1298
Rubén Fernández Vega <rfernandez@nubestic.com>
2026-06-08 00:22:51 +0200

    feat: descarga de contenidos en cloud pages y analisis

--- src/renderer/ui/dom-elements.js
+++ src/renderer/ui/dom-elements.js
@@ -359,14 +359,7 @@ export function init() {
     elements.cloudPageIdsImportBtn = document.getElementById('cloudpage-ids-import-btn');
 
     // --- 21. MODAL DE CONTENIDOS DE CLOUD PAGES ---
-    elements.getCloudPageContentsBtn = document.getElementById('getCloudPageContentsBtn');
-    elements.cloudPageContentsModal = document.getElementById('cloudpage-contents-modal-overlay');
-    elements.cloudPageCbLink = document.getElementById('cloudpage-cb-link');
-    elements.cloudPageFetchScript = document.getElementById('cloudpage-fetch-script');
-    elements.cloudPageCopyScriptBtn = document.getElementById('cloudpage-copy-script-btn');
-    elements.cloudPageContentsPasteArea = document.getElementById('cloudpage-contents-paste-area');
-    elements.cloudPageContentsCancelBtn = document.getElementById('cloudpage-contents-cancel-btn');
-    elements.cloudPageContentsImportBtn = document.getElementById('cloudpage-contents-import-btn');
+    /*Modal eliminado*/
 
     // --- 22. GESTOR DE CONTENIDOS ---
     elements.importNewContentBtn = document.getElementById('importNewContentBtn');
